0de733a637a87903bfbaf6c74c5ac167037660aa
Squip <skip@skip.sx>
2025-06-18 01:02:13 -0700

    adding legacy codebase directories for hypertuna-desktop, hypertuna-worker

---
+++ hypertuna-desktop-LEGACY/crypto-libraries.js
@@ -0,0 +1,25 @@
+/**
+ * crypto-libraries.js
+ * Load cryptographic libraries for Nostr operations
+ */
+
+// Import the libraries directly
+import * as secp256k1 from 'noble-secp256k1';
+import * as cipher from 'browserify-cipher';
+import * as bech32 from 'bech32';
+
+
+// Assign them to the window object for compatibility with existing code
+window.nobleSecp256k1 = secp256k1;
+window.browserifyCipher = cipher;
+window.bech32 = bech32;
+
+// Export the libraries for module imports
+export { bech32, secp256k1 as nobleSecp256k1, cipher as browserifyCipher};
+
+// Log that libraries were loaded successfully
+console.log('Crypto libraries loaded successfully:');
+console.log('Noble Secp256k1 loaded:', !!window.nobleSecp256k1);
+console.log('Browserify Cipher loaded:', !!window.browserifyCipher);
+console.log('Bech32 loaded:', !!window.bech32);
+
